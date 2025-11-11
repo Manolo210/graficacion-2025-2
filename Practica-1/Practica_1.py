ce20fd2308ad7fb071ce6c89787f640a54d5de61
Manolo210 <manel05232@gmail.com>
2025-11-11 13:24:54 -0800

    Corrección Practica 1

--- Practica-1/Practica_1.py
+++ Practica-1/Practica_1.py
@@ -28,35 +28,35 @@ while running:
     if teclas[pygame.K_LSHIFT]:
         print("Tecla shift presionada")
 
-
+    #Aumentamos la velocidad a la derecha
     if teclas[pygame.K_RIGHT] and teclas[pygame.K_LSHIFT]:
         x += vel+2
     if teclas[pygame.K_RIGHT] and teclas[pygame.K_LSHIFT]:
         if pygame.KEYUP:
              x += vel
         print('Ambas teclas son presionadas')
-
+    # Aumentamos la velocidad a la izquierda
     if teclas[pygame.K_LEFT] and teclas[pygame.K_LSHIFT]:
         x -= vel + 2
     if teclas[pygame.K_LEFT] and teclas[pygame.K_LSHIFT]:
         if pygame.KEYUP:
             x -= vel
         print('Ambas teclas son presionadas')
-
+    # Aumentamos la velocidad hacia arriba
     if teclas[pygame.K_UP] and teclas[pygame.K_LSHIFT]:
         y -= vel + 2
     if teclas[pygame.K_UP] and teclas[pygame.K_LSHIFT]:
         if pygame.KEYUP:
             y -= vel
         print('Ambas teclas son presionadas')
-
+    # Aumentamos la velocidad Hacia abajo
     if teclas[pygame.K_DOWN] and teclas[pygame.K_LSHIFT]:
         y += vel + 2
     if teclas[pygame.K_DOWN] and teclas[pygame.K_LSHIFT]:
         if pygame.K_DOWN:
             y += vel
         print('Ambas teclas son presionadas')
-
+    #limites para que el cuadrado no se pase de la pantalla establecida
     if x < 0:
         x = 0
     if x + ancho > 600:
@@ -67,8 +67,8 @@ while running:
         y = 400 - alto
 
     pantalla.fill((30, 30, 30))
-    pygame.draw.rect(pantalla, (250,250,250), (x, y, ancho, alto))
-
+    pygame.draw.rect(pantalla, (250,250,250), (x, y, ancho, alto)) #Cambamos color del personaje
+    #Creamos las paredes
     pygame.draw.rect(pantalla, ( 0,255, 0), (0, 398, 600,2))
     pygame.draw.rect(pantalla, (0, 255, 0), (0, 1, 2, 400))
     pygame.draw.rect(pantalla, (0, 255, 0), (0, -1, 600, 3))
